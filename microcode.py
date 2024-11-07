4a5a6fde3ee0d8bd18611aa683e78b9e890f0c56
Aidan McKenna <aidan.mckenna@paritygo.com>
2024-11-07 16:59:03 -0500

    add microcode pins

--- microcode.py
+++ microcode.py
@@ -1,7 +1,15 @@
 
 import yaml
 
-with open("./config.yaml", r) as f:
+with open("./config.yaml", 'r') as f:
 	cfg = yaml.load(f, Loader=yaml.Loader);
 
+print(cfg)
+def load_control_bits():
+	control_bits = {}
+	one = 1
+	for i in range(len(cfg['control_bits'])):
+		control_bits[cfg['control_bits'][i]] = one << i
+	
 
+load_control_bits()
